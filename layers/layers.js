1a7fc5cbf4ce1e78a82365bc707f14393964a094
nortonpcmaciel <nortonperes@gmail.com>
2026-06-16 15:22:49 -0300

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,43 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleTerrain_0 = new ol.layer.Tile({
+            'title': 'Google Terrain',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
+                url: 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'
+            })
+        });
+var format_JARDIMMORI_1 = new ol.format.GeoJSON();
+var features_JARDIMMORI_1 = format_JARDIMMORI_1.readFeatures(json_JARDIMMORI_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JARDIMMORI_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JARDIMMORI_1.addFeatures(features_JARDIMMORI_1);
+var lyr_JARDIMMORI_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_JARDIMMORI_1, 
+                style: style_JARDIMMORI_1,
+                popuplayertitle: 'JARDIM MORIÁ',
+                interactive: true,
+    title: 'JARDIM MORIÁ<br />\
+    <img src="styles/legend/JARDIMMORI_1_0.png" /> DISPONÍVEL<br />\
+    <img src="styles/legend/JARDIMMORI_1_1.png" /> VERDE<br />\
+    <img src="styles/legend/JARDIMMORI_1_2.png" /> INSTITUCIONAL<br />\
+    <img src="styles/legend/JARDIMMORI_1_3.png" /> QUITADO<br />\
+    <img src="styles/legend/JARDIMMORI_1_4.png" /> VENDIDOS<br />\
+    <img src="styles/legend/JARDIMMORI_1_5.png" /> BLOQUEADO<br />\
+    <img src="styles/legend/JARDIMMORI_1_6.png" /> <br />' });
+
+lyr_GoogleTerrain_0.setVisible(true);lyr_JARDIMMORI_1.setVisible(true);
+var layersList = [lyr_GoogleTerrain_0,lyr_JARDIMMORI_1];
+lyr_JARDIMMORI_1.set('fieldAliases', {'QUADRA': 'QUADRA', 'LOTE': 'LOTE', 'QDLT': 'QDLT', 'STATUS': 'STATUS', });
+lyr_JARDIMMORI_1.set('fieldImages', {'QUADRA': 'Range', 'LOTE': 'Range', 'QDLT': 'TextEdit', 'STATUS': 'TextEdit', });
+lyr_JARDIMMORI_1.set('fieldLabels', {'QUADRA': 'header label - always visible', 'LOTE': 'header label - always visible', 'QDLT': 'hidden field', 'STATUS': 'hidden field', });
+lyr_JARDIMMORI_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/JARDIMMORI_1_style.js
@@ -0,0 +1,89 @@
+var size = 0;
+var placement = 'point';
+function categories_JARDIMMORI_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'D':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(255,255,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'G':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(77,175,74,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'I':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(253,191,111,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Q':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(255,150,148,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'V':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(89,200,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'B':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.38}),fill: new ol.style.Fill({color: 'rgba(248,246,247,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(235,77,167,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.608}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_JARDIMMORI_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("STATUS");
+    var labelFont = "13.0px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_JARDIMMORI_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
